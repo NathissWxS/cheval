658c9e8659ad4b45202fbc37007acfd3764ebaa0
Jorys Pephily <jorys.pephily@coda-student.school>
2026-02-03 15:43:35 +0100

    front

--- front/src/hooks/RequireAuth.tsx
+++ front/src/hooks/RequireAuth.tsx
@@ -1,35 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navigate, Outlet } from "react-router";
 
 export default function RequireAuth() {
-  const [isAuthenticated, setisAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
 
-  //let isAuthenticated = false;
-
-  fetch("http://localhost:8000/api/me", {
-    method: "GET",
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-    credentials: "include",
-  })
-    .then((response) => {
-      if (response.status === 200) {
-        setisAuthenticated(true);
-
-        if (!isAuthenticated) {
-          console.log("User has a problem " + isAuthenticated);
-          return <Navigate to="/login" replace />;
-        } else {
-          console.log("User is authenticated");
-        }
-      } else {
-        throw new Error("Failed to fetch user data");
-      }
+  useEffect(() => {
+    fetch("http://localhost:8000/api/me", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+      credentials: "include",
     })
-    .catch((error) => {
-      console.error("Error fetching user data:", error);
-    });
+      .then((response) => {
+        if (response.status === 200) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching user data:", error);
+        setIsAuthenticated(false);
+      });
+  }, []);
+
+  if (isAuthenticated === null) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
 
   return <Outlet />;
 }
--- front/src/pages/armurerie.tsx
+++ front/src/pages/armurerie.tsx
@@ -5,10 +5,29 @@ export default function Armurerie() {
   const [items, setItems] = useState([]);
 
   useEffect(() => {
-    fetch("/api/items")
+    fetch("http://localhost:8000/api/equipements", {
+      method: "GET",
+      credentials: "include",
+    })
       .then((response) => response.json())
       .then((data) => setItems(data));
   }, []);
+
+  function HandleEquip(id: number, isEquip: boolean) {
+    if (isEquip) {
+      fetch(`http://localhost:8000/api/chevalier/equipements/equiper`, {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify({ equipement_id: id }),
+      });
+    } else {
+      fetch(`http://localhost:8000/api/chevalier/equipements/retirer`, {
+        method: "DELETE",
+        credentials: "include",
+        body: JSON.stringify({ equipement_id: id }),
+      });
+    }
+  }
 
   return (
     <>
@@ -22,7 +41,18 @@ export default function Armurerie() {
 
       <ul>
         {items.map((item) => (
-          <li key={item.id}>{item.name}</li>
+          <>
+            <li key={item.id}>{item.nom}</li>
+            {item.equiped ? (
+              <button onClick={() => HandleEquip(item.id, false)}>
+                Déséquiper
+              </button>
+            ) : (
+              <button onClick={() => HandleEquip(item.id, true)}>
+                Equiper
+              </button>
+            )}
+          </>
         ))}
       </ul>
     </>
